c0fc37f00afa1617917c7735eeb0d9d3276c83ab
Thiago L. A. Miller <tmiller@mochsl.org.br>
2019-02-19 04:34:32 -0300

    main.c: Add hypergeometric test

--- src/main.c
+++ src/main.c
@@ -26,16 +26,24 @@ main (int argc, char *argv[])
 	if (argc == 1)
 		return 0;
 
+	// Precisa-se de 3 arquivos:
+	// A: Universe
+	// B: Max Success
+	// C: Observed Success
 	int rc = 0;
-	Comm *comm = NULL;
+	Comm *comm1_2 = NULL;
+	Comm *comm2_3 = NULL;
+	Comm *comm1_3 = NULL;
 	PtrArray *array1 = NULL;
 	PtrArray *array2 = NULL;
-	/*PtrArray *array3 = NULL;*/
+	PtrArray *array3 = NULL;
 
-	comm = comm_new (NULL, &rc);
+	comm1_2 = comm_new (NULL, &rc);
+	comm2_3 = comm_new (NULL, &rc);
+	comm1_3 = comm_new (NULL, &rc);
 	array1 = ptr_array_new (free, &rc);
 	array2 = ptr_array_new (free, &rc);
-	/*array3 = ptr_array_new (free, &rc);*/
+	array3 = ptr_array_new (free, &rc);
 
 	if (rc != E_SUCCESS)
 		{
@@ -81,74 +89,82 @@ main (int argc, char *argv[])
 
 	assert (rc == 0);
 
-	/*rc = read_file_lines (array3, argv[3]);*/
-	/*if (rc != E_SUCCESS)*/
-		/*{*/
-			/*error (0, rc, "Failed to read file '%s'", argv[3]);*/
-			/*goto Exit;*/
-		/*}*/
+	rc = read_file_lines (array3, argv[3]);
+	if (rc != E_SUCCESS)
+		{
+			error (0, rc, "Failed to read file '%s'", argv[3]);
+			goto Exit;
+		}
 
-	/*assert (rc == 0);*/
+	assert (rc == 0);
 
-	/*if (array3->len == 0)*/
-		/*{*/
-			/*rc = set_error_code (D_LOCAL, E_NOLINE);*/
-			/*error (0, rc, "Failed to read file '%s'", argv[3]);*/
-			/*goto Exit;*/
-		/*}*/
+	if (array3->len == 0)
+		{
+			rc = set_error_code (D_LOCAL, E_NOLINE);
+			error (0, rc, "Failed to read file '%s'", argv[3]);
+			goto Exit;
+		}
 
-	/*assert (rc == 0);*/
+	assert (rc == 0);
 
 	ptr_array_uniq (array1, cmpstringp);
 	ptr_array_uniq (array2, cmpstringp);
-	/*qsort (array3->pdata, array3->len, sizeof (char *), cmpstringp);*/
+	ptr_array_uniq (array3, cmpstringp);
 
-	rc = compare_arrays (comm, array1, array2, cmpstringp);
+	rc = compare_arrays (comm1_2, array1, array2, cmpstringp);
 	if (rc != E_SUCCESS)
 		{
 			error (0, rc, "Error during comparing files '%s' and '%s'", argv[1], argv[2]);
 			goto Exit;
 		}
 
-	printf ("Uniq lines to %s\n", argv[1]);
-	for  (int i = 0;  i < comm->uniq_to_file1->len; i++)
-		printf ("- %s\n", (char *) ptr_array_get (comm->uniq_to_file1, i));
+	rc = compare_arrays (comm2_3, array2, array3, cmpstringp);
+	if (rc != E_SUCCESS)
+		{
+			error (0, rc, "Error during comparing files '%s' and '%s'", argv[2], argv[3]);
+			goto Exit;
+		}
 
-	printf ("Uniq lines to %s\n", argv[2]);
-	for  (int i = 0;  i < comm->uniq_to_file2->len; i++)
-		printf ("- %s\n", (char *) ptr_array_get (comm->uniq_to_file2, i));
+	rc = compare_arrays (comm1_3, array1, array3, cmpstringp);
+	if (rc != E_SUCCESS)
+		{
+			error (0, rc, "Error during comparing files '%s' and '%s'", argv[1], argv[3]);
+			goto Exit;
+		}
 
-	printf ("Shared lines between %s and %s\n", argv[1], argv[2]);
-	for  (int i = 0;  i < comm->shared->len; i++)
-		printf ("- %s\n", (char *) ptr_array_get (comm->shared, i));
+	/*printf ("Uniq lines to %s\n", argv[1]);*/
+	/*for  (int i = 0;  i < comm->uniq_to_file1->len; i++)*/
+		/*printf ("- %s\n", (char *) ptr_array_get (comm1_2->uniq_to_file1, i));*/
+
+	/*printf ("Uniq lines to %s\n", argv[2]);*/
+	/*for  (int i = 0;  i < comm->uniq_to_file2->len; i++)*/
+		/*printf ("- %s\n", (char *) ptr_array_get (comm1_2->uniq_to_file2, i));*/
+
+	/*printf ("Shared lines between %s and %s\n", argv[1], argv[2]);*/
+	/*for  (int i = 0;  i < comm->shared->len; i++)*/
+		/*printf ("- %s\n", (char *) ptr_array_get (comm1_2->shared, i));*/
+
+	int N = array1->len;
+	int K = comm1_2->shared->len;
+	int n = comm1_3->shared->len;
+	int k = comm2_3->shared->len;
+
+	hyper_s h;
+	hyper (&h, N, K, n, k);
+
+	printf ("[N=%d K=%d n=%d k=%d]\n", N, K, n, k);
+	printf ("P(N eq %d) = %.*lf\n", k, N_DIGITS, h.pmf);
+	printf ("P(N lt %d) = %.*lf\n", k, N_DIGITS, h.cdfe);
+	printf ("P(N le %d) = %.*lf\n", k, N_DIGITS, h.cdfi);
+	printf ("P(N gt %d) = %.*lf\n", k, N_DIGITS, 1 - h.cdfi);
+	printf ("P(N ge %d) = %.*lf\n", k, N_DIGITS, 1 - h.cdfe);
 
 Exit:
-	comm_free (comm, 1);
+	comm_free (comm1_2, 1);
+	comm_free (comm2_3, 1);
+	comm_free (comm1_3, 1);
 	ptr_array_free (array1, 1);
 	ptr_array_free (array2, 1);
-	/*ptr_array_free (array3, 1);*/
+	ptr_array_free (array3, 1);
 	return rc == E_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-/*int*/
-/*main (int argc, char *argv[])*/
-/*{*/
-	/*if (argc != 5)*/
-		/*error (1, set_error_code (D_LOCAL, E_NOARG), "Usage %s N K n k", PACKAGE);*/
-
-	/*int N = atoi (argv[1]);*/
-	/*int K = atoi (argv[2]);*/
-	/*int n = atoi (argv[3]);*/
-	/*int k = atoi (argv[4]);*/
-
-	/*hyper_s h;*/
-	/*hyper (&h, N, K, n, k);*/
-
-	/*printf ("[N=%d K=%d n=%d k=%d]\n", N, K, n, k);*/
-	/*printf ("P(N eq %d) = %.*lf\n", k, N_DIGITS, h.pmf);*/
-	/*printf ("P(N lt %d) = %.*lf\n", k, N_DIGITS, h.cdfe);*/
-	/*printf ("P(N le %d) = %.*lf\n", k, N_DIGITS, h.cdfi);*/
-	/*printf ("P(N gt %d) = %.*lf\n", k, N_DIGITS, 1 - h.cdfi);*/
-	/*printf ("P(N ge %d) = %.*lf\n", k, N_DIGITS, 1 - h.cdfe);*/
-
-	/*return 0;*/
-/*}*/
